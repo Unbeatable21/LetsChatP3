861e81005d6f25131f1d5363b59a4d8842cec27d
Unbeatable21 <96412028+Unbeatable21@users.noreply.github.com>
2023-08-14 19:27:42 +1000

    Add files via upload

---
+++ room.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB1FKMpr2SYRC5QD6bx85uRD5KS0QZS00E",
+    authDomain: "letschat-a5a94.firebaseapp.com",
+    projectId: "letschat-a5a94",
+    storageBucket: "letschat-a5a94.appspot.com",
+    messagingSenderId: "491699348900",
+    appId: "1:491699348900:web:e74fd2e05c94373562cd31"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
